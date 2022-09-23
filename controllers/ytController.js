cfb98205d558f0a51453f6563cfb0d081087ab88
vinigiu <vinigiu@hotmail.com>
2022-09-23 14:54:11 -0300

    Ajustando caminho do save do vídeo para quem baixar o repositório no GitHub

--- controllers/ytController.js
+++ controllers/ytController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 const youtubeDownload = (link) => {
-    ytdl.getBasicInfo(link)        
+    ytdl.getBasicInfo(link)  // retirando resources/app/ do caminho do save do vídeo para que funcione para quem baixa no github     
         .then(a => ytdl(link).pipe(fs.createWriteStream(`resources/app/downloaded-videos/${a.videoDetails.videoId}.mp4`)))
 }
 
